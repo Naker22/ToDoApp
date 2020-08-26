81b51f3f868d3e794ab91fb387fe1a998a5a15d3
Stepan Tchynetskyi <60452439+Naker22@users.noreply.github.com>
2020-08-26 22:51:47 +0300

    prefinal version

--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import ToDo
+from django.http import HttpResponseRedirect
 # Create your views here.
 def home(request):
-	return render(request,'BASE.html')
+	todo_items = ToDo.objects.all().order_by("-added_date")
+	return render(request,'main/index.html',{"todo_items":todo_items})
+
+def add_todo(request):
+	added_date = timezone.now()
+	content = request.POST['content']
+	ToDo.objects.create(added_date = added_date,text = content)
+	return HttpResponseRedirect("/")
+
+def delete_todo(request,todo_id):
+	ToDo.objects.get(id=todo_id).delete()
+	return HttpResponseRedirect('/')
